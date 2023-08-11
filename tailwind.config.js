e6c3c386207693b682304bbf775278037c0aa593
Olivia Li <oliviali.ouyang@gmail.com>
2023-08-11 16:56:13 -0700

    remove dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/*.{js,ts,jsx,tsx,mdx}', './containers/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))',
-        },
-      },
-      padding: {
-        30: '7.5rem', // 120px
-      }
-    },
-  },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/*.{js,ts,jsx,tsx,mdx}",
+    "./containers/*.{js,ts,jsx,tsx,mdx}",
+  ],
   plugins: [],
 }
